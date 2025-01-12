364f45ab39862a327ceff90b0495c788dcbff89b
Hugocl01 <hugocayon@gmail.com>
2025-01-12 19:45:14 +0100

    Comparador Fixes #23

---
+++ js/comparador.js
@@ -0,0 +1,85 @@
+'use strict';
+
+import { guardarDatosPokemon, existeDatosEnGeneraciones, mostrarSpinner, ocultarSpinner, mostrarFichaPokemon, limpiarDatosPokemon } from "./utils.js";
+import { app } from "./main.js";
+
+async function cargaInicial() {
+    mostrarSpinner();
+    const datosExisten = await existeDatosEnGeneraciones();
+
+    if (!datosExisten) {
+        const generaciones = await app.obtenerDatosPokemon();
+
+        if (generaciones) {
+            await guardarDatosPokemon(generaciones);
+        }
+    }
+
+    const pokemons = await app.obtenerDatosDesdeIndexedDB('todos');
+
+    if (pokemons) {
+        mostrarFichaPokemon(pokemons);
+    }
+
+    ocultarSpinner();
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // Recoger elementos del HTML
+    const inputNombrePokemon = document.getElementById('buscarNombre');
+    const selectGeneracion = document.getElementById('generationFilter');
+    const contenedorFichas = document.querySelector('.contenedor-fichas');
+
+    // Al cargar realizar la obtención de los datos y mostrar pokémons
+    await cargaInicial();
+
+    // EVENTO - Select generación
+    selectGeneracion.addEventListener('change', async (event) => {
+        //mostrarSpinner();
+        let pokemons;
+        const generacionSeleccionada = event.target.value;
+
+        if (generacionSeleccionada === 'all') {
+            pokemons = await app.obtenerDatosDesdeIndexedDB('todos');
+        } else {
+            pokemons = await app.obtenerDatosDesdeIndexedDB('generacion', generacionSeleccionada);
+        }
+
+        if (pokemons) {
+            mostrarFichaPokemon(pokemons);
+        }
+
+        //ocultarSpinner();
+    });
+
+    // EVENTO - Contenedor búsqueda (botón 'Buscar' y 'Limpiar')
+    document.querySelector('.contenedor-busqueda-nombre').addEventListener('click', async (event) => {
+        if (event.target && event.target.tagName === 'INPUT' && event.target.type === 'button') {
+            const accion = event.target.dataset.accion;
+
+            switch (accion) {
+                case 'buscar':
+                    selectGeneracion.value = 'all';
+                    const pokemon = await app.obtenerDatosDesdeIndexedDB('name', inputNombrePokemon.value);
+
+                    if (pokemon) {
+                        mostrarFichaPokemon(pokemon);
+                    }
+                    break;
+                case 'limpiar':
+                    contenedorFichas.innerHTML = '';
+                    inputNombrePokemon.value = '';
+                    selectGeneracion.value = 'all';
+                    await cargaInicial();
+                    break;
+            }
+        }
+    });
+
+    // EVENTO - vaciar tabla 'generaciones' de IndexedDB (para pruebas en desarrollo)
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'F2') {
+            limpiarDatosPokemon();
+        }
+    });
+});

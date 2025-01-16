db85c53fcd847a4a9a5710af64fedc41f8f220bf
victorllada <victor.sanchezllada@iesmiguelherrero.com>
2025-01-16 18:55:26 +0100

    Modificación carrusel index Fixes #37

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,46 +1,43 @@
 const carrusel = document.querySelector('.carrusel-imagenes');
-const images = document.querySelectorAll('.carrusel-imagenes img');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-let index = 0;
+const image = document.querySelector('.carrusel-imagenes img');
+//const prevButton = document.querySelector('.prev');
+//const nextButton = document.querySelector('.next');
 
 // Función para actualizar el carrusel
-function updateCarrusel() {
-    const width = images[0].clientWidth;
-    carrusel.style.transform = `translateX(${-index * width}px)`;
+function actualizarCarrusel() {
+    const numeroAleatorio = Math.floor(Math.random() * 1025) + 1; // Genera un número aleatorio entre 1 y 1025
+    image.src = '../img/logoPokeball.png'; // Cambia la imagen a la Pokéball
+    carrusel.style.transform = 'scale(0.5)';
+    setTimeout(() => {
+        image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${numeroAleatorio}.png`;
+        carrusel.style.transform = 'scale(1)';
+    }, 700); // Cambia a la nueva imagen después de 700ms
 }
 
-// Función para avanzar al siguiente slide
-function nextSlide() {
-    index = (index + 1) % images.length;
-    updateCarrusel();
-}
-
-// Función para retroceder al slide anterior
-function prevSlide() {
-    index = (index - 1 + images.length) % images.length;
-    updateCarrusel();
+// Función para generar otro slide
+function cambiarSlide() {
+    actualizarCarrusel();
 }
 
 // Botones de navegación manual
-nextButton.addEventListener('click', () => {
-    nextSlide();
-    resetAutoSlide(); // Reinicia el temporizador de desplazamiento automático
+/*nextButton.addEventListener('click', () => {
+    cambiarSlide();
+    reiniciarAutoSlide(); // Reinicia el temporizador de desplazamiento automático
 });
 
 prevButton.addEventListener('click', () => {
-    prevSlide();
-    resetAutoSlide(); // Reinicia el temporizador de desplazamiento automático
-});
+    cambiarSlide();
+    reiniciarAutoSlide(); // Reinicia el temporizador de desplazamiento automático
+});*/
 
 // Desplazamiento automático
-let autoSlide = setInterval(nextSlide, 3000); // Cambia cada 3 segundos
+let autoSlide = setInterval(cambiarSlide, 3000); // Cambia cada 3 segundos
 
 // Reinicia el temporizador de desplazamiento automático
-function resetAutoSlide() {
+function reiniciarAutoSlide() {
     clearInterval(autoSlide);
-    autoSlide = setInterval(nextSlide, 3000);
+    autoSlide = setInterval(cambiarSlide, 3000);
 }
 
-// Asegura que el carrusel se actualice al cambiar el tamaño de la ventana
-window.addEventListener('resize', updateCarrusel);
+// Inicializa el carrusel con una imagen aleatoria al cargar la página
+actualizarCarrusel();
